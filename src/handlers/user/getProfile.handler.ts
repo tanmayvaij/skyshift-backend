01cad27065daa30dbd78a7f468aea6bc6be9ebd6
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-04-21 21:33:31 +0530

    added get profile route
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/handlers/user/getProfile.handler.ts
+++ src/handlers/user/getProfile.handler.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { UserModel } from "../../models";
 
 export const getProfile = async (req: Request, res: Response) => {
-  const userProfile = await UserModel.findOne({ _id: req.user.userId }, { password: 0 });
+  const userProfile = await UserModel.findOne({ _id: req.user.userId }, { password: 0, __v: 0 });
 
   res.json({ status: true, userProfile });
 };
